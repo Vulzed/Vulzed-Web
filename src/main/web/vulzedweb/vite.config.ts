fe286badf29c958e3a49d130f731f1e77451d998
Swordzi <github@sword.mozmail.com>
2022-08-06 17:52:46 +0300

    Build config

--- src/main/web/vulzedweb/vite.config.ts
+++ src/main/web/vulzedweb/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import {build, defineConfig} from 'vite'
+import 'vite/modulepreload-polyfill'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
+
+
 })
+
+build.assetsDir = '../../resources/static/assets'
+build.outputDir = '../../resources/templates'
